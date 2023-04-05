320b3036066152975ec40d1ad8ee99d71efa924d
Limsungrae <tjdfo7879@gmail.com>
2023-04-05 15:16:14 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(requst):
+    return render(
+        requst,
+        'single_pages/landing.html'
+    )
+def about_me(requst):
+    return render(
+        requst,
+        'single_pages/about_me.html'
+    )
