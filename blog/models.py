6f456323b9743ea88e17b5e8eda40c82db06b6fd
Limsungrae <tjdfo7879@gmail.com>
2023-04-06 18:20:53 +0900

    4/6일 마무리

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 import os
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __srt__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = '카테고리'
+    
+
 class Post (models.Model) :
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -14,15 +25,16 @@ class Post (models.Model) :
     updatede_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User,null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True,blank=True,  on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
     
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
-    
+    #upload 파일 이름 가져오기
     def get_file_name(self):
         return os.path.basename(self.file_upload.name)
-    
+    #upload 파일 속성 가져오기
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
