01cda9cd5ec48ffedfe11bf7ddb857c7dafd63a1
Limsungrae <tjdfo7879@gmail.com>
2023-04-07 17:46:56 +0900

    태그 페이지 만들기

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from .models import Post
-from .models import Category
+from .models import Category,Tag
 
 admin.site.register(Post)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name', )}
 
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+
+
+
 
 # Register your models here.
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
 # Create your views here.
 # def index(request) :
 #     posts = Post.objects.all().order_by('-pk')
@@ -12,6 +12,40 @@ from .models import Post, Category
 #             'posts':posts,
 #         }
 #     )
+def tag_page(request,slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list= tag.post_set.all()
+
+    return render(
+            request,
+            'blog/post_list.html',
+            {
+                'post_list':post_list,
+                'tag': tag,
+                'categories': Category.objects.all(),
+                'no_category_post_count':Post.objects.filter(category=None).count(),
+            }
+        )
+
+def category_page(request,slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+            request,
+            'blog/post_list.html',
+            {
+                'post_list': post_list,
+                'categories':Category.objects.all(),
+                'no_category_post_count': Post.objects.filter(category=None).count(),
+                'category': category,
+            }
+        )
+        
 class PostList(ListView):
     model = Post
     ordering = '-pk'
@@ -21,12 +55,34 @@ class PostList(ListView):
         context['categories'] =  Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
+    
+    # def category_page(request, slug):
+    #     context ={}
+    #     category = Category.objects.get(slug=slug)
+    #     context['post_list'] = Post.objects.filter(category=category)
+    #     context['categories'] =  Category.objects.all()
+    #     context['no_category_post_count'] = Post.objects.filter(category=None).count()
+    #     context['category'] = category
+
+    #     return render(
+    #         request,
+    #         'blog/post_list.html',
+    #         context
+    #     )
+
+
+
 
 
 
 
 class PostDetail(DetailView):
     model = Post
+    def get_context_data(self, **kwargs ):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] =  Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
 # def single_post_page(request, pk) :
 #     post = Post.objects.get(pk=pk)
